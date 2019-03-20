c91b093059097b7296e12a3c8bf1a23fd9eb8828
userman95 <orestismail71@gmail.com>
2019-03-20 15:14:03 +0200

    Beautify the page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'home' => 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get '/signup' => 'users#new'
+  get '/home'     =>  'static_pages#home'
+  get '/help'     =>   'static_pages#help'
+  get '/about'    =>  'static_pages#about'
+  get '/contact'  =>  'static_pages#contact'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
