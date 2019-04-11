7d948c8c201a5b09838bf21f26c944b9504cb516
userman95 <orestismail71@gmail.com>
2019-04-11 16:40:53 +0300

    adding session and cookies but also tests for all occasions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:edit,:update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def show
@@ -18,9 +26,35 @@ class UsersController < ApplicationController
      flash.now[:danger] = "Could not save client"
      render 'new'
    end
- end
+  end
 
- def user_params
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated successfully"
+      redirect_to @user
+    else
+      flash[:danger] = "Profile not updated"
+      render 'edit'
+    end
+  end
+
+  def logged_in_user
+   unless logged_in?
+     flash[:danger]="Please log in first"
+     redirect_to login_url
+   end
+  end
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  private
+
+  def user_params
    params.require(:user).permit(:name,:email,:password,:password_confirmation)
- end
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,25 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  test "should redirect edit when not logged in" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    puts login_url
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
 end
